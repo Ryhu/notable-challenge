26a472871dbd436eb857712ca4eee7fd55417892
unknown <ryanhuuu@gmail.com>
2020-08-05 14:21:53 -0400

    added patient, doctor, appointment models

---
+++ db/migrate/20200805131011_create_appointments.rb
@@ -0,0 +1,12 @@
+class CreateAppointments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :appointments do |t|
+      t.string :datetime
+      t.string :type
+      t.integer :doctor_id
+      t.integer :patient_id
+
+      t.timestamps
+    end
+  end
+end

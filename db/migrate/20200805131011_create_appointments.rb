97512ae72adcb113aac4ee313fbd378f7c2deb5c
unknown <ryanhuuu@gmail.com>
2020-08-05 15:54:08 -0400

    oopsies, drop db, remigrate

--- db/migrate/20200805131011_create_appointments.rb
+++ db/migrate/20200805131011_create_appointments.rb
@@ -1,7 +1,7 @@
 class CreateAppointments < ActiveRecord::Migration[6.0]
   def change
     create_table :appointments do |t|
-      t.string :datetime
+      t.datetime :datetime
       t.string :type
       t.integer :doctor_id
       t.integer :patient_id

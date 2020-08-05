32c4225ef74d5fc47dba13e2e9db89d4d8e91b18
unknown <ryanhuuu@gmail.com>
2020-08-05 15:41:09 -0400

    controllers

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,46 @@
 class AppointmentsController < ApplicationController
+  before_action :set_appointment, only: [:show, :update, :destroy]
+
+  # GET /appointments
+  def index
+    @appointment = Appointment.where(doctor_id: params[:id])
+    @appointment = @appointment.filter {|appointment| appointment.datetime.to_date === params[:date]} 
+    json_response(@appointment)
+  end
+
+  # POST /appointment
+  def create
+    # creating an appointment would also create a patient, or pull one from the db
+    new_patient = 'patient'
+    # patient needs an uniq identifier, ie social security / email
+    # because time, patients will have to just...have the same first and last name. it's terrible
+    @patient = Patient.find(first_name: appointment_params[:first_name], last_name: appointment_params[:last_name])
+
+    if @patient
+      @patient = Patient.create(first_name: appointment_params[:first_name], last_name: appointment_params[:last_name])
+      new_patient = 'follow-up'
+    end
+
+    # make params more dynamic later
+    @appointment = Appointment.create(datetime: appointment_params[:datetime], patient_id: @patient.id, doctor_id: params[:id], type: new_patient)
+
+    json_response(@appointment, :created)
+  end
+
+  # DELETE /appointment/:id
+  def destroy
+    @appointment.destroy
+    head :no_content
+  end
+
+  private
+
+  def appointment_params
+    # whitelist params
+    params.permit(:title, :created_by)
+  end
+
+  def set_appointment
+    @appointment = Appointments.find(params[:id])
+  end
 end
--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,22 @@
 class DoctorsController < ApplicationController
+  before_action :set_doctor, only: [:appointments]
+  def index
+    @doctors = Doctor.all
+    json_response(@doctors)
+  end
+
+  def appointments
+    json_response(@doctor.appointments)
+  end
+
+  private
+
+  def doctor_params
+    # whitelist params
+    params.permit(:first_name, :last_name)
+  end
+
+  def set_doctor
+    @doctor = Doctor.find(params[:id])
+  end
 end
